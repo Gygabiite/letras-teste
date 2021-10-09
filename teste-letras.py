a468b0a0805e7bb6212bfa1397e0644f10ddcb17
Gygabiite <68558179+Gygabiite@users.noreply.github.com>
2021-10-09 12:08:06 -0300

    Desduplicador 2.0
    Desduplicador com nova lógica que resolve todos os bugs que eu encontrei.

--- teste-letras.py
+++ teste-letras.py
@@ -3,43 +3,46 @@ class Teste:
         self.entrada = entrada
 
     def desDuplicar(self):
-        #Cria uma variável para resposta
+        #Cria uma string vazia para guardar a resposta.
         resposta = ""
-        #Loopa entre todas as palavras da string informada
+        #Loopa entre todas as palavras da string informada.
         for palavra in self.entrada.split(" "):
-            temp = ""
-            temp2 = ""
-            #Loopa por cada letra na palavra ao contrário
-            for letra in reversed(palavra):
-                #Verifica se a letra atual já ocorreu antes, se sim, adiciona ela na string temporária 2, se não adiciona na string temporária 1
-                #Se a letra já ocorreu antes em ambas as strings temporárias, adiciona ela na 1
-                if letra in temp and letra not in temp2:
-                    temp2 += letra
-                    if temp2 == temp:
+            #Variável boolean para verificar se a duplicação foi eliminada.
+            confirmaPalavraComDuplicacao = False
+            #Verifica se o tamanho da palavra é impar ou par, porque se for impar tem que eliminar a primeira letra, pois a mesma não vai ter duplicação
+            if len(palavra) % 2 == 0:
+                #Loopa pela metade do tamanho da palavra, verificando se substrings da mesma são iguais
+                for i in range(int(len(palavra)/2)):
+                    if palavra.removesuffix(palavra[int(len(palavra)/2) + i:])[-int(len(palavra[int(len(palavra)/2) + i:])):] == palavra[int(len(palavra)/2) + i:]:
+                        #Se as substrings forem iguais, elimina a duplicação da palavra e confirma que foi eliminada
+                        resposta += palavra.removesuffix(palavra[int(len(palavra) / 2) + i:]) + " "
+                        confirmaPalavraComDuplicacao = True
                         break
-                else:
-                    temp += letra
-            
-            #Verificando se ouve alguma duplicação, se não apenas retorna a string dada no começo.
-            if len(temp2) == 0:
+            else:
+                #Loopa pela metade do tamanho da palavra, verificando se substrings da mesma são iguais
+                for i in range(int(len(palavra[1:])/2)):
+                    if palavra[1:].removesuffix(palavra[1:][int(len(palavra[1:])/2) + i:])[-int(len(palavra[1:][int(len(palavra[1:])/2) + i:])):] == palavra[1:][int(len(palavra[1:])/2) + i:]:
+                        #Se as substrings forem iguais, elimina a duplicação da palavra e confirma que foi eliminada
+                        resposta += palavra.removesuffix(palavra[1:][int(len(palavra[1:])/2) + i:]) + " "
+                        confirmaPalavraComDuplicacao = True
+                        break
+            #Se alguma palavra não houver duplicação, retorna a string como ela entrou.               
+            if confirmaPalavraComDuplicacao == False:
                 return self.entrada.strip() + "."
-            #Adiciona a palavra sem a duplicação no final
-            resposta += palavra.removesuffix(temp2[::-1]) + " "
-        #Retorna a string completa sem a duplicaçõe no final de cada palavra
+        #Se todas as palavras tiverem duplicação, retorna a nova string sem as duplicações com um ponto no final
         return resposta.strip() + "."
+
 
 #Instanciando a classe com os casos de teste
 teste1 = Teste("oo ratoato roeuoeu aa roupaoupa dodo reiei dee romaoma")
 teste2 = Teste("banana")
 teste3 = Teste("a bananeira tem banana")
 teste4 = Teste("saarara temem duas bolass")
-teste5 = Teste("araraa")
-teste6 = Teste(str(input("Frase: ")))
+teste5 = Teste("araraara")
 
-#Outputs
+#Chamando o método de desduplicação e mostrando o resultado
 print(teste1.desDuplicar())
 print(teste2.desDuplicar())
 print(teste3.desDuplicar())
 print(teste4.desDuplicar())
 print(teste5.desDuplicar())
-print(teste6.desDuplicar())
